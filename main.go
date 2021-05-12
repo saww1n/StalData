e9f698ce29df34b9d2a9ed0c378638b932222f32
saww1n <46725666+saww1n@users.noreply.github.com>
2021-05-12 23:13:42 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(page, "Hi")
+}
+
 // main go
 func main() {
-
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
 }
